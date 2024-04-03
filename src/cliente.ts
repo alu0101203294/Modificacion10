a950f677105773e87345b2635f912eca5352724a
alu0101203294 <alu0101203294@ull.edu.es>
2024-04-03 09:44:33 +0000

    Documentacion generada con TypeDoc

--- src/cliente.ts
+++ src/cliente.ts
@@ -1,24 +1,45 @@
 import * as net from 'net';
 import { argv } from 'process';
 
+/**
+ * Crea una conexión con el servidor y envía el archivo especificado por argumento de comando.
+ * @param {string} argv[2] - Ruta del archivo a enviar al servidor
+ * @returns {void}
+ * 
+ */
 const client = net.connect({ port: 60300 }, () => {
   console.log('Conectado al servidor.');
 
+  /**
+   * Si se especifica un archivo por argumento de comando, se envía al servidor.
+   * 
+   */
   if (argv.length > 2) {
     const filePath = argv[2];
     client.write(filePath);
   }
 });
 
+/**
+ * Muestra en consola la respuesta del servidor.
+ * 
+ */
 client.on('data', (data) => {
   console.log(data.toString());
   client.end();
 });
 
+/**
+ * Muestra en consola que se ha desconectado del servidor.
+ * 
+ */
 client.on('end', () => {
   console.log('Desconectado del servidor.');
 });
 
+/**
+ * Muestra en consola si hay un error.
+ */
 client.on('error', (error) => {
   console.error(`Error: ${error.message}`);
 });
--- src/servidor.ts
+++ src/servidor.ts
@@ -2,22 +2,52 @@ import * as net from 'net';
 import { exec } from 'child_process';
 
 
-export const server = net.createServer((connection) => {
+/**
+ * Servidor que recibe la ruta de un archivo y devuelve el número de líneas, palabras y caracteres.
+ * @param connection - Conexión con el cliente
+ * @returns {void}
+ * 
+ */
+const server = net.createServer((connection) => {
   console.log('Cliente conectado.');
 
+  /**
+   * Recibe la ruta del archivo y devuelve el número de líneas, palabras y caracteres.
+   * @param data - Ruta del archivo
+   * @returns {void}
+   * 
+   */
   connection.on('data', (data) => {
     const filePath = data.toString().trim();
 
+    /**
+     * Ejecuta el comando wc para contar el número de líneas, palabras y caracteres del archivo.
+     * @param filePath - Ruta del archivo
+     * @returns {void}
+     * 
+     */
     exec(`cat "${filePath}" | wc`, (error, stdout, stderr) => {
+      /**
+       * Si hay un error, se envía un mensaje de error al cliente.
+       */
       if (error) {
         connection.write(`Error: ${error.message}`);
         return;
       }
+      /**
+       * Si hay un error en la salida estándar, se envía un mensaje de error al cliente.
+       */
       if (stderr) {
         connection.write(`Error: ${stderr}`);
         return;
       }
 
+      /**
+       * Se separa la salida estándar en líneas, palabras y caracteres.
+       * Se arregla el número de líneas para que sea correcto. (linea + 1)
+       * Se envía el resultado al cliente.
+       * 
+       */
       const [lineas, palabras, caracteres] = stdout.split(/\s+/).filter(Boolean);
       const arreglarnumerolineas = parseInt(lineas) + 1;
       const resultado = `\nResultado:\nLíneas: ${arreglarnumerolineas}\nPalabras: ${palabras}\nCaracteres: ${caracteres}\n`;
@@ -25,11 +55,17 @@ export const server = net.createServer((connection) => {
     });
   });
 
+  /**
+   * Muestra en consola que el cliente se ha desconectado.
+   */
   connection.on('close', () => {
     console.log('Cliente desconectado.');
   });
 });
 
-server.listen(60300, () => {
-  console.log('Servidor escuchando en el puerto 60300.');
-});
+  /**
+   * Muestra en consola que el servidor está escuchando en el puerto 60300.
+   */
+  server.listen(60300, () => {
+    console.log('Servidor escuchando en el puerto 60300.');
+  });
